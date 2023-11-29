297855a3c3478db6dec5f842bc53fba95973f904
wOL-Lucas <lucasefernandes333@gmail.com>
2023-11-28 23:15:57 -0300

    Implementing AuthenticationController

---
+++ spring_api/src/main/java/parf/api/controller/AuthenticationController.java
@@ -0,0 +1,43 @@
+package parf.api.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import parf.api.Entities.User.AuthenticationDTO;
+import parf.api.Entities.User.RegisterDTO;
+import parf.api.Entities.User.User;
+import parf.api.repositories.UserRepository;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository repository;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
+        if(this.repository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.login(), encryptedPassword, data.role());
+        this.repository.save(newUser);
+
+        return ResponseEntity.ok().build();
+    }
+}
