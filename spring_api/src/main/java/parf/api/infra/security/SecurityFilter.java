00c1ef8f74ed9b21f252d71a35b4fa7169eb4637
wOL-Lucas <lucasefernandes333@gmail.com>
2023-11-29 20:48:19 -0300

    Create SecurityFilter.java

---
+++ spring_api/src/main/java/parf/api/infra/security/SecurityFilter.java
@@ -0,0 +1,46 @@
+package parf.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import parf.api.Services.TokenService;
+import parf.api.repositories.UserRepository;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if(token != null){
+            var subject = tokenService.validateToken(token);
+            UserDetails user = userRepository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var token = request.getHeader("Authorization");
+        if(token == null || token.isBlank() || !token.startsWith("Bearer ")) return null;
+        return token.replace("Bearer ", "");
+    }
+}
