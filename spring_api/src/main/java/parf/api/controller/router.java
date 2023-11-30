c00966ad9485e301036dc69c06617a77dd0350a3
wOL-Lucas <lucasefernandes333@gmail.com>
2023-11-30 03:39:25 -0300

    Fix: Cors policy to localhost

--- spring_api/src/main/java/parf/api/controller/router.java
+++ spring_api/src/main/java/parf/api/controller/router.java
@@ -1,10 +1,21 @@
 package parf.api.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import parf.api.Entities.Account.Account;
+import parf.api.Entities.Products.Product;
+import parf.api.repositories.ProductsRepository;
 
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api/v1/")
 public class router {
+
+    @Autowired
+    ProductsRepository productsRepository;
 
     @GetMapping
     public String controller(){
@@ -14,5 +25,20 @@ public class router {
     @GetMapping("/public")
     public String publicAccess(){
         return "public";
+    }
+
+    @GetMapping("/products")
+    public List<Product> products(){
+        return productsRepository.findAll();
+    }
+
+    @PostMapping("/products")
+    public Product createProduct(@RequestBody @Valid Product product){
+        return productsRepository.save(product);
+    }
+
+    @GetMapping("/account")
+    public Account Account(){
+        return new Account("00001-0");
     }
 }
